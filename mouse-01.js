20ff6283903a3b242a3e40f3e0f24cdcef6963ae
Julia Ortiz Naharro <jortizna@beta03.aulas.gsyc.urjc.es>
2019-04-04 11:06:04 +0200

    ya hace todo menos el reloj

--- mouse-01.js
+++ mouse-01.js
@@ -1,29 +1,68 @@
 function main()
 {
   console.log("Probando...")
+  d = document.getElementById('display1')
+  video1 = document.getElementById('video1')
+  video2 = document.getElementById("video2")
+  video3 = document.getElementById("video3")
+//-- Establecer el tamaño del vídeo
+  video1.width = 200;
+  video1.height= 100;
+  video2.width = 200;
+  video2.height= 100;
+  video3.width = 200;
+  video3.height= 100;
 
-  v = document.getElementById('video1')
-  v.width = 200;
-  v.height= 100;
-  v.onmouseover = () => {
+
+  video1.onmouseover = () => {
     console.log("Mouse over video1!!")
     //MUSICCCC
+    video1.muted = false;
+    console.log("Musica video1!!")
   }
+  video1.onmouseout = () => {
+    console.log("Mouse out video1!!")
+    //MUSICCCC
+    video1.muted = true;
+    console.log("Musica video1!!")
+  }
+  video2.onmouseover = () => {
+    console.log("Mouse over video2!!")
+    //MUSICCCC
+    video2.muted = false;
+    console.log("Musica video2!!")
+  }
+  video2.onmouseout = () => {
+    console.log("Mouse out video2!!")
+    //MUSICCCC
+    video2.muted = true;
+    console.log("Musica video2!!")
+  }
+  video3.onmouseover = () => {
+    console.log("Mouse over video3!!")
+    //MUSICCCC
+    video3.muted = false;
+    console.log("Musica video3!!")
+  }
+  video3.onmouseout = () => {
+    console.log("Mouse OUT video3!!")
+    //MUSICCCC
+    video3.muted = true;
+    console.log("Musica video3!!")
+  }
+
+  v = document.getElementById('display')
+  v.width = 600;
+  v.height= 300;
+
+
+
   ver1 = document.getElementById('play1')
   ver1.onclick = () => {
     console.log("Ver!")
-    v.src = "video-src-01-robot.mp4"
+    v.src = "circo.mp4"
+    v.currentTime = video1.currentTime
 }
-  console.log("Video 02...")
- //-- Obtener el elemento de video
- video2 = document.getElementById("video2")
- video2.onmouseover = () => {
-   console.log("Mouse over video2!!")
-   //MUSICCCC
- }
- //-- Establecer el tamaño del vídeo
- video2.width = 200;
- video2.height= 100;
    //-- Boton de Play
  ver2 = document.getElementById('play2')
 
@@ -32,6 +71,18 @@ function main()
    console.log("Ver!")
 
   //-- Indicar la fuente del vídeo
-  video2.src ="https://gsyc.urjc.es/jmplaza/csaai/realizador-fuente1.mp4"
+  v.src ="https://gsyc.urjc.es/jmplaza/csaai/realizador-fuente1.mp4"
+  v.currentTime = video2.currentTime
   }
+   //-- Boton de Play
+ ver3 = document.getElementById('play3')
+
+ //-- Al apretar el boton de ver video
+ ver3.onclick = () => {
+   console.log("Ver!")
+
+  //-- Indicar la fuente del vídeo
+  v.src ="piano.mp4"
+  v.currentTime = video3.currentTime
+}
 }
